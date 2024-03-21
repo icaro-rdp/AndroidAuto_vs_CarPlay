14b4747a23ef0ef17dbe2b645a3c1444d91ace15
Icaro Re Depaolini <igher97@gmail.com>
2024-03-21 17:30:11 +0100

    pandas conversion

--- gaze_analysis/main.py
+++ gaze_analysis/main.py
@@ -1,1 +1,11 @@
 # Here there will be the code for analyzing the gaze data 
+import os
+import pandas as pd
+
+file_paths = "gaze_analysis/data"
+
+for file in os.listdir(file_paths):
+    if file.endswith(".txt"):
+        gaze_data = pd.read_table(os.path.join(file_paths, file), sep = "\t")
+
+
