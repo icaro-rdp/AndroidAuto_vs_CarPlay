faeca343797b3e0cd4461ecf6a01c5c7635b4504
Icaro Re Depaolini <igher97@gmail.com>
2024-04-02 15:32:29 +0200

    Add gaze analysis functions and generate summary CSV

--- gaze_analysis/main.py
+++ gaze_analysis/main.py
@@ -2,10 +2,41 @@
 import os
 import pandas as pd
 
-file_paths = "gaze_analysis/data"
+# Analyzing the gaze data to determine if the gaze is distracted or not based on the gaze value and a threshold value, if the gaze value is greater than the threshold value then the gaze is not distracted else it is distracted
+def gaze_analysis(gaze_data,threshold):
+    gaze_data["distracted"] = gaze_data["gaze_value"].apply(lambda x: "no" if x>threshold else "yes")
+    return gaze_data
 
+# Counting the number of distracting gazes for the entire video by counting the number of times the gaze switches from distracted to not distracted or vice versa
+def count_distracting_gazes(gaze_analysis_data):
+    count = 0
+    prev = gaze_analysis_data["distracted"][0] 
+    for i in range(len(gaze_data)):
+        if gaze_analysis_data["distracted"][i] != prev:
+            count += 1
+            prev = gaze_analysis_data["distracted"][i]
+    return count
+
+file_paths = "gaze_analysis/data"
+threshold = 0.9
+final_csv = pd.DataFrame()
 for file in os.listdir(file_paths):
     if file.endswith(".txt"):
-        gaze_data = pd.read_table(os.path.join(file_paths, file), sep = "\t")
+        gaze_data = pd.read_table(os.path.join(file_paths, file), sep = "," ,names=["frame_id","gaze_value"])
+        
+        gaze_analysis_data = gaze_analysis(gaze_data,threshold)
+        distracting_gazes_count = count_distracting_gazes(gaze_analysis_data)
+        
+        summary_df = gaze_analysis_data["gaze_value"].describe().to_frame().T
+        summary_df.insert(0, "ID", file.split(".")[0])
+        summary_df.insert(1,"distracting_gazes_count", distracting_gazes_count)
+        final_csv = pd.concat([final_csv, summary_df])
+
+final_csv.to_csv("gaze_analysis/gaze_analysis_summary.csv", index=False)
+    
 
 
+        
+
+    
+    
