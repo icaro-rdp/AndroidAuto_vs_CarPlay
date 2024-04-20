e6ee6389f0af5861774190f74d638ac9ff5fbabd
Icaro Re Depaolini <igher97@gmail.com>
2024-04-20 17:14:04 +0200

    Update gaze analysis data files and add questionnaire module

--- gaze_analysis/main.py
+++ gaze_analysis/main.py
@@ -2,6 +2,7 @@
 import os
 import pandas as pd
 import json
+import questionnaire
 
 # Analyzing the gaze data to determine if the gaze is distracted or not based on the gaze value and a threshold value, if the gaze value is greater than the threshold value then the gaze is not distracted else it is distracted
 def gaze_analysis(gaze_data,threshold):
@@ -12,54 +13,56 @@ def gaze_analysis(gaze_data,threshold):
 def count_distracting_gazes(gaze_analysis_data):
     count = 0
     prev = gaze_analysis_data["distracted"][0] 
-    for i in range(len(gaze_data)):
+    for i in range(len(gaze_analysis_data)):
         if gaze_analysis_data["distracted"][i] != prev:
             count += 1
             prev = gaze_analysis_data["distracted"][i]
     return count
 
-# Reading the game data from the files and analyzing the game data
-games_paths = "gaze_analysis/games_data"
-games_df = pd.DataFrame()
-for file in os.listdir(games_paths):
-     if file.endswith(".json"):
-         with open(os.path.join(games_paths, file)) as json_file:
-            game_data = json.load(json_file)
-            game_data.popitem()
-            game_df = pd.DataFrame.from_dict(game_data, orient='index').T
-            games_df = pd.concat([games_df, game_df])
+def main():
+    # Reading the game data from the files and analyzing the game data
+    games_paths = "gaze_analysis/games_data"
+    games_df = pd.DataFrame()
+    for file in os.listdir(games_paths):
+        if file.endswith(".json"):
+            with open(os.path.join(games_paths, file)) as json_file:
+                game_data = json.load(json_file)
+                game_data.popitem()
+                game_df = pd.DataFrame.from_dict(game_data, orient='index').T
+                games_df = pd.concat([games_df, game_df])
 
-# Reading the gaze data from the files and analyzing the gaze data             
-file_paths = "gaze_analysis/data"
-threshold = 0.9
-gazes_df = pd.DataFrame()
-for file in os.listdir(file_paths):
-    if file.endswith(".txt"):
-        gaze_data = pd.read_table(os.path.join(file_paths, file), sep = "," ,names=["frame_id","gaze_value"])
-        
-        gaze_analysis_data = gaze_analysis(gaze_data,threshold)
-        distracting_gazes_count = count_distracting_gazes(gaze_analysis_data)
-        percentage_of_distracting_gazes = gaze_analysis_data["distracted"].value_counts(normalize=True)["yes"].round(3)
-        amount_of_seconds_distracted = percentage_of_distracting_gazes * len(gaze_data) / 30
-        
-        
-        summary_df = gaze_analysis_data["gaze_value"].describe()[["mean","std","min","25%","50%","75%","max"]].to_frame().transpose()
-        file_id = file.split(".")[0]
-        summary_df.insert(0, "subjectID", file_id.split("_")[0])
-        summary_df.insert(1, "platformID", file_id.split("_")[1])
-        summary_df.insert(2, "taskID", file_id.split("_")[2])
-        summary_df.insert(3,"video_duration", len(gaze_data)/30)
-        summary_df.insert(4,"distracting_gazes_count", distracting_gazes_count)
-        summary_df.insert(5, "percentage_of_distracting_gazes", percentage_of_distracting_gazes)
-        summary_df.insert(6, "amount_of_seconds_distracted", amount_of_seconds_distracted)
+    # Reading the gaze data from the files and analyzing the gaze data             
+    file_paths = "gaze_analysis/data"
+    threshold = 0.9
+    gazes_df = pd.DataFrame()
+    for file in os.listdir(file_paths):
+        if file.endswith(".txt"):
+            gaze_data = pd.read_table(os.path.join(file_paths, file), sep = "," ,names=["frame_id","gaze_value"])
+            
+            gaze_analysis_data = gaze_analysis(gaze_data,threshold)
+            distracting_gazes_count = count_distracting_gazes(gaze_analysis_data)
+            percentage_of_distracting_gazes = gaze_analysis_data["distracted"].value_counts(normalize=True)["yes"].round(3)
+            amount_of_seconds_distracted = percentage_of_distracting_gazes * len(gaze_data) / 30
+            
+            
+            summary_df = gaze_analysis_data["gaze_value"].describe()[["mean","std","min","25%","50%","75%","max"]].to_frame().transpose()
+            file_id = file.split(".")[0]
+            summary_df.insert(0, "subjectID", file_id.split("_")[0])
+            summary_df.insert(1, "platformID", file_id.split("_")[1])
+            summary_df.insert(2, "taskID", file_id.split("_")[2])
+            summary_df.insert(3,"video_duration", len(gaze_data)/30)
+            summary_df.insert(4,"distracting_gazes_count", distracting_gazes_count)
+            summary_df.insert(5, "percentage_of_distracting_gazes", percentage_of_distracting_gazes)
+            summary_df.insert(6, "amount_of_seconds_distracted", amount_of_seconds_distracted)
 
-        gazes_df = pd.concat([gazes_df, summary_df])
-        
+            gazes_df = pd.concat([gazes_df, summary_df])
+            
 
-summary_df = pd.merge(gazes_df, games_df, on=["subjectID","platformID","taskID"], how="left")
-summary_df.to_csv("gaze_analysis/gaze_analysis_data.csv", index=False)
-         
-
+    summary_df = pd.merge(gazes_df, games_df, on=["subjectID","platformID","taskID"], how="left")
+    summary_df.to_csv("gaze_analysis/analysis_outputs/gaze_analysis_data.csv", index=False)
+            
+if __name__ == "__main__":
+    main()
     
 
 
---
+++ gaze_analysis/questionnaire.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+
+def transform_NaN_to_col_avg(df, col_name):
+    if pd.api.types.is_numeric_dtype(df[col_name]):
+        col_avg = df[col_name].mean()
+        df[col_name] = df[col_name].fillna(col_avg)
+    return df
+
+
+def transform_questionnaire_df(questionnaire_df):
+    
+    participants_columns = ["Name","Surname","Age","Gender","Phone_type","license_years","AA_fam","CP_fam",]
+    
+    participants_df = questionnaire_df.iloc[:, 3:11]
+    participants_df.columns = participants_columns
+
+    
+    platforms_columns = ["eou_1","eou_2","eou_3","umux_1","umux_2","umux_3","umux_4","umux_5","umux_6","umux_7","umux_8","umux_9","umux_10"]
+
+    android_df = questionnaire_df.iloc[:, 11:24]
+    android_df.columns = platforms_columns
+    apple_df = questionnaire_df.iloc[:, 24:37]
+    apple_df.columns = platforms_columns
+
+   
+    new_questionnaire_df = pd.concat([participants_df, android_df, apple_df], axis = 1)
+
+    for col in new_questionnaire_df.columns:
+        new_questionnaire_df = transform_NaN_to_col_avg(new_questionnaire_df, col)
+        
+    return new_questionnaire_df
+
+def main():
+    questionnaire_paths = "gaze_analysis/questionnaire_data/questionnaire.csv"
+    questionnaire_df = pd.read_csv(questionnaire_paths, sep = ",")
+
+    transformed_questionnaire_df = transform_questionnaire_df(questionnaire_df)
+
+    transformed_questionnaire_df.to_csv("gaze_analysis/questionnaire_data/transformed_questionnaire.csv", index=False)
+    
+
+if __name__ == "__main__":
+    main()
+
