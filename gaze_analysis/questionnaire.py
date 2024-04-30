4acca3b5aabbc773c6fe6d3dd3a87b35ac94c0e0
Icaro Re Depaolini <igher97@gmail.com>
2024-04-30 16:20:44 +0200

    Update transformed_questionnaire.csv and questionnaire.py

--- gaze_analysis/questionnaire.py
+++ gaze_analysis/questionnaire.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import math
 
 # Function to transform possible NaN values in a column to the average of the column if the column is numeric
 def transform_NaN_to_col_avg(df, col_name):
     if pd.api.types.is_numeric_dtype(df[col_name]):
-        col_avg = df[col_name].mean()
+        col_avg = math.floor(df[col_name].mean())
         df[col_name] = df[col_name].fillna(col_avg)
     return df
 
@@ -17,12 +17,12 @@ def transform_questionnaire_df(questionnaire_df):
     participants_df.columns = participants_columns
 
     
-    platforms_columns = ["eou_1","eou_2","eou_3","sus_1","sus_2","sus_3","sus_4","sus_5","sus_6","sus_7","sus_8","sus_9","sus_10"]
-
+    aa_columns = ["AA_eou_1","AA_eou_2","AA_eou_3","AA_sus_1","AA_sus_2","AA_sus_3","AA_sus_4","AA_sus_5","AA_sus_6","AA_sus_7","AA_sus_8","AA_sus_9","AA_sus_10"]
+    cp_columns = ["CP_eou_1","CP_eou_2","CP_eou_3","CP_sus_1","CP_sus_2","CP_sus_3","CP_sus_4","CP_sus_5","CP_sus_6","CP_sus_7","CP_sus_8","CP_sus_9","CP_sus_10"]
     android_df = questionnaire_df.iloc[:, 11:24]
-    android_df.columns = platforms_columns
+    android_df.columns = aa_columns 
     apple_df = questionnaire_df.iloc[:, 24:37]
-    apple_df.columns = platforms_columns
+    apple_df.columns = cp_columns
 
    
     new_questionnaire_df = pd.concat([participants_df, android_df, apple_df], axis = 1)
