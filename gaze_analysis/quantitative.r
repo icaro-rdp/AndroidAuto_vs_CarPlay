58500ddcc6f27325781f50bc148573dd63d431d7
Icaro Re Depaolini <igher97@gmail.com>
2024-05-08 13:08:07 +0200

    refactor: Update gaze_analysis module for quantitative analysis

--- gaze_analysis/quantitative.r
+++ gaze_analysis/quantitative.r
@@ -1,30 +1,44 @@
+library(car)
+library(carData)
+library(ggplot2)
 # Import data
 df <- read.csv("gaze_analysis_data.csv")
 
+df <- df[df$taskID != "T0", ]
 tot <- df[df$subjectID != "26", ]$time_on_task
 
 upper_bound <- boxplot.stats(tot)$stats[5]
 lower_bound <- boxplot.stats(tot)$stats[1]
 
-mean_tot_AA_t1 <- mean(df$time_on_task[df$platformID == "AA" & df$taskID == "T1"])
-mean_tot_CP_t1 <- mean(df$time_on_task[df$platformID == "CP" & df$taskID == "T1"])
+# Create boxplot for time on task by platform
+ggplot(df, aes(x = platformID, y = time_on_task)) +
+    geom_boxplot(fill = c("skyblue", "lightgreen"), color = c("darkblue", "darkgreen")) +
+    xlab("Platform") +
+    ylab("Time on task") +
+    ggtitle("Time on task by platform")
 
-mean_tot_AA_t2 <- mean(df$time_on_task[df$platformID == "AA" & df$taskID == "T2"])
-mean_tot_CP_t2 <- mean(df$time_on_task[df$platformID == "CP" & df$taskID == "T2"])
+ggsave("time_on_task_plat.png")
+# Create boxplot for time on task by task
+ggplot(df, aes(x = taskID, y = time_on_task)) +
+    geom_boxplot(fill = c("#F7766D", "#00BB38", "#629DFF"), color = c("darkred", "darkgreen", "darkblue")) +
+    xlab("Task") +
+    ylab("Time on task") +
+    ggtitle("Time on task by task")
 
-mean_tot_AA_t3 <- mean(df$time_on_task[df$platformID == "AA" & df$taskID == "T3"])
-mean_tot_CP_t3 <- mean(df$time_on_task[df$platformID == "CP" & df$taskID == "T3"])
+ggsave("time_on_task_by_task.png")
 
-print(paste("Mean time on task for AA in T1: ", mean_tot_AA_t1))
-print(paste("Mean time on task for CP in T1: ", mean_tot_CP_t1))
+# Create boxplot for time on task by platform and task
+ggplot(df, aes(x = platformID, y = time_on_task, fill = taskID)) +
+    geom_boxplot() +
+    xlab("Platform") +
+    ylab("Time on task") +
+    ggtitle("Time on task by platform and task")
 
-print(paste("Mean time on task for AA in T2: ", mean_tot_AA_t2))
-print(paste("Mean time on task for CP in T2: ", mean_tot_CP_t2))
-
-print(paste("Mean time on task for AA in T3: ", mean_tot_AA_t3))
-print(paste("Mean time on task for CP in T3: ", mean_tot_CP_t3))
-
-# Perform t-test
+ggsave("time_on_task_boxplot.png")
 
 # Check if CP is faster than AA in T3
 t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T3"], df$time_on_task[df$platformID == "CP" & df$taskID == "T3"], alternative = "less", paired = TRUE, var.equal = FALSE)
+
+# Anova for time on task by platform and task
+aov_time_on_task <- aov(time_on_task ~ platformID * taskID, data = df)
+print(summary(aov_time_on_task))
