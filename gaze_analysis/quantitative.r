6f82117f978cc6044e594705ac1fe27ec5499741
Icaro Re Depaolini <igher97@gmail.com>
2024-05-09 14:47:03 +0200

    fasafa

--- gaze_analysis/quantitative.r
+++ gaze_analysis/quantitative.r
@@ -1,6 +1,7 @@
 library(car)
 library(carData)
 library(ggplot2)
+library(ggcorrplot)
 # Import data
 setwd("/Users/icaroredepaolini/Personale/uni/AndroidAuto_vs_CarPlay/gaze_analysis/analysis_outputs")
 
@@ -41,24 +42,35 @@ ggplot(df, aes(x = platformID, y = time_on_task, fill = taskID)) +
 
 ggsave("time_on_task_boxplot.png")
 
-# Check if CP is faster than AA in T3
-t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T1"], df$time_on_task[df$platformID == "CP" & df$taskID == "T1"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
-
-t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T2"], df$time_on_task[df$platformID == "CP" & df$taskID == "T2"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
-
-t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T3"], df$time_on_task[df$platformID == "CP" & df$taskID == "T3"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
-
 # Anova for time on task by platform and task
-aov_time_on_task <- aov(time_on_task ~ platformID * taskID, data = df)
-aov_game_error <- aov(average_game_error ~ platformID * taskID, data = df)
+
+aov_time_on_task <- Anova(lm(time_on_task ~ platformID * taskID, data = df), type = "III")
+
+aov_game_error <- Anova(lm(average_game_error ~ platformID * taskID, data = df), type = "III")
+
+t_T1 <- t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T1"], df$time_on_task[df$platformID == "CP" & df$taskID == "T1"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
+
+t_T2 <- t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T2"], df$time_on_task[df$platformID == "CP" & df$taskID == "T2"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
+
+t_T3 <- t.test(df$time_on_task[df$platformID == "AA" & df$taskID == "T3"], df$time_on_task[df$platformID == "CP" & df$taskID == "T3"], alternative = "two.sided", paired = TRUE, var.equal = FALSE)
+
+# P-value correction
+number_of_tests <- 3
+alpha <- 0.05
+p_adjust <- alpha / number_of_tests
+
+# Check if the t-test is significant
+print(t_T1$p.value < p_adjust)
+print(t_T2$p.value < p_adjust)
+print(t_T3$p.value < p_adjust) # Only T3 is significant
 
 # Check cor btw time on task and game error
 cor(df$time_on_task, df$average_game_error)
-plot(df$time_on_task, df$average_game_error)
 # Check task and game error by platform
 cor(df$time_on_task[df$platformID == "AA"], df$average_game_error[df$platformID == "AA"])
 
 cor(df$time_on_task[df$platformID == "CP"], df$average_game_error[df$platformID == "CP"])
+
 
 # Check cor btw time on task and game error by task
 cor(df$time_on_task[df$taskID == "T1"], df$average_game_error[df$taskID == "T1"])
